3c07ca7330551aeabcc37d1fece07eec07019b8e
곽재헌 <honeykwak@gwagjaeheon-ui-MacBookPro.local>
2025-08-01 21:22:29 +0900

    change headers background color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,10 @@ import ColorGuide from './ui/ColorGuide'
 
 export default function Header() {
   return (
-    <div className="flex-shrink-0 bg-white border-b border-gray-200 px-6 py-3">
+    <div className="flex-shrink-0 bg-neutral-600 border-b border-gray-200 px-6 py-3">
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-6">
-          <h1 className="h1-title text-gray-900">
+          <h1 className="h1-title text-white">
             ContextualAllianceVIS
           </h1>
         </div>
